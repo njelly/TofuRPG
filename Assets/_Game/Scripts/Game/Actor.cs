7b7f823da38a9689d62fd13733f8c830a3758b28
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-07-22 00:35:31 -0600

    working on components

---
+++ Assets/_Game/Scripts/Game/Actor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuRPG.Game
+{
+    public class Actor : MonoBehaviour
+    {
+        public ActorInput Input { get; private set; }
+        public bool IsReceivingInput { get; private set; }
+
+        [SerializeField] private bool _recieveOnStart;
+
+        private void Start()
+        {
+            if (_recieveOnStart)
+            {
+                BeginReceivingInput();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            StopReceivingInput();
+        }
+
+        public void BeginReceivingInput()
+        {
+            ActorInputManager.AddTarget(this);
+            IsReceivingInput = true;
+        }
+
+        public void StopReceivingInput()
+        {
+            ActorInputManager.RemoveTarget(this);
+            IsReceivingInput = false;
+            Input = null;
+        }
+
+        public void ReceiveInput(ActorInput input)
+        {
+            Input = input;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Game/GridMover.cs
@@ -0,0 +1,44 @@
+﻿using Tofunaut.TofuUnity;
+using UnityEngine;
+using static Tofunaut.TofuUnity.TofuAnimator;
+
+namespace Tofunaut.TofuRPG.Game
+{
+    public class GridMover : MonoBehaviour
+    {
+        public Vector2Int Coord { get; private set; }
+
+        public float moveSpeed;
+
+        private Actor _actor;
+        private TofuAnimator.Sequence _moveSequence;
+
+        private void Awake()
+        {
+            _actor = gameObject.RequireComponent<Actor>();
+        }
+
+        private void Update()
+        {
+            if (_moveSequence == null && _actor.Input.direction.sqrMagnitude <= float.Epsilon)
+            {
+            }
+
+        }
+
+        private void MoveTo(Vector2Int newCoord)
+        {
+            Vector2Int prevCoord = Coord;
+            Coord = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+
+
+            _moveSequence = gameObject.Sequence()
+                .Curve(EEaseType.Linear, moveSpeed, (float newValue) =>
+                {
+                    //transform.position = Vector2.LerpUnclamped(prevCoord, )
+
+                });
+            _moveSequence.Play();
+        }
+    }
+}
