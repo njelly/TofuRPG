4eadc440c224c906a7e017fa0b520517149cb991
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-08-06 20:35:29 -0600

    created SignPost interactable

---
+++ Assets/_Game/Scripts/Game/SignPost.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.TofuRPG.Game
+{
+    public class SignPost : MonoBehaviour, Interactor.IInteractable
+    {
+        [TextArea] public string _dialog;
+
+        public void BeginInteraction(Interactor interactor)
+        {
+            Debug.Log(_dialog);
+        }
+
+        public void EndInteraction(Interactor interactor)
+        {
+        }
+    }
+}
