e8e35eadfc32d246d31fc2f4e5349b2751a7001e
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-08-08 11:10:39 -0600

    created Pokeable, Dummy prefab that plays SFX

---
+++ Assets/_Game/Scripts/Game/Pokeable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Tofunaut.TofuRPG.Game
+{
+    // an IInteractable that triggers some arbitray event *poke*
+    public class Pokeable : MonoBehaviour, Interactor.IInteractable
+    {
+        public UnityEvent OnBeginInteraction;
+        public UnityEvent OnEndInteraction;
+
+        public void BeginInteraction(Interactor interactor)
+        {
+            OnBeginInteraction?.Invoke();
+        }
+
+        public void EndInteraction(Interactor interactor)
+        {
+            OnEndInteraction?.Invoke();
+        }
+    }
+}
