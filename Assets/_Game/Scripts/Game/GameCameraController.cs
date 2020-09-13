eae2e93aa38f0f8eeb7f42f48fbdf9faa7ffd91f
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-09-13 10:09:29 -0600

    working on rebuilding core actor-component systems

--- Assets/_Game/Scripts/Game/GameCameraController.cs
+++ Assets/_Game/Scripts/Game/GameCameraController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace Tofunaut.TofuRPG.Game
 {
+    [ExecuteInEditMode]
     [RequireComponent(typeof(Camera))]
     public class GameCameraController : SingletonBehaviour<GameCameraController>
     {
@@ -20,6 +21,7 @@ namespace Tofunaut.TofuRPG.Game
 
         [SerializeField] private GameObject _following;
         [SerializeField] private Vector2 _offset;
+        [SerializeField] private float _cameraZ = -10;
 
         private Camera _camera;
         private Vector2 _followPos;
@@ -37,8 +39,12 @@ namespace Tofunaut.TofuRPG.Game
             {
                 _followPos = _following.transform.position;
             }
+            else
+            {
+                _followPos = new Vector3(transform.position.x, transform.position.y, _cameraZ);
+            }
 
-            gameObject.transform.position = new Vector3(_offset.x, _offset.y, -10f) + new Vector3(_followPos.x, _followPos.y, 0f);
+            gameObject.transform.position = new Vector3(_offset.x, _offset.y, _cameraZ) + new Vector3(_followPos.x, _followPos.y, 0f);
         }
 
         public static void Follow(GameObject target)
