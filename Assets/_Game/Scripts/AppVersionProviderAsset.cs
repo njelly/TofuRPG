259807859dbe646dd2e506c0fb886aed7adc7877
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-10-31 14:03:53 -0600

    created AppVersionProviderAsset

---
+++ Assets/_Game/Scripts/AppVersionProviderAsset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tofunaut.TofuUnity;
+using UnityEngine;
+using Version = Tofunaut.TofuUnity.Version;
+
+namespace Tofunaut.TofuRPG
+{
+    [CreateAssetMenu(fileName = "AppVersionProviderAsset", menuName = "TofuRPG/DataAsset/AppVersionProviderAsset", order = 1)]
+    public class AppVersionProviderAsset : ScriptableObject, IVersionProvider
+    {
+        public Version Version =>
+            _version ?? (_version =
+                new Version($"{Application.version}{Version.Delimiter}{BuildNumberUtil.ReadBuildNumber()}"));
+
+        private Version _version;
+    }
+}
---
+++ Assets/_Game/Scripts/UI/AppVersionLabel.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Tofunaut.TofuRPG.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class AppVersionLabel : MonoBehaviour
+    {
+        public AppVersionProviderAsset appVersionProviderAsset;
+        
+        private TextMeshProUGUI _text;
+
+        private void Awake()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            _text.text = string.Empty;
+        }
+
+        private void Start()
+        {
+            _text.text = appVersionProviderAsset.Version.ToString();
+        }
+    }
+
+}
